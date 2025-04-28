0c9bae6c9ed6bdf69188a96097484c4b1f672ecf
Vovdenko Dmitriy <vov-dm-an@yandex-team.ru>
2025-04-28 21:00:33 +0300

    Logger optimization made

--- hydrolib_logger/include/hydrolib_log_distributor.hpp
+++ hydrolib_logger/include/hydrolib_log_distributor.hpp
@@ -28,7 +28,11 @@ namespace hydrolib::Logger
         template <typename... Ts>
         void Notify(unsigned source_id, Log &log, Ts... params) const
         {
-            distributing_list_.head_node->Notify(source_id, log, params...);
+            if (distributing_list_.head_node->Notify(source_id, log.level))
+            {
+                log.ToBytes(format_string_, *distributing_list_.head_node,
+                            params...);
+            }
         }
 
         hydrolib_ReturnCode SetFilter(unsigned stream_number, unsigned logger_id,
@@ -48,17 +52,14 @@ namespace hydrolib::Logger
                                           Streams_...>
         {
         public:
-            consteval LogDistributingNode_(char *format_string,
-                                           NextNode *next_node,
+            consteval LogDistributingNode_(NextNode *next_node,
                                            Stream &stream,
                                            Streams_ &...streams)
                 : LogDistributingNode_<LogDistributingNode_<NextNode, Stream, Streams_...>,
-                                       Streams_...>(format_string,
-                                                    this, streams...),
+                                       Streams_...>(this, streams...),
                   output_stream_(stream),
-                  format_string_(format_string),
-                  next_node_(next_node),
-                  stream_enable_(false)
+                  stream_enable_(false),
+                  next_node_(next_node)
             {
                 for (size_t i = 0; i < MAX_LOGGERS_COUNT; i++)
                 {
@@ -88,30 +89,23 @@ namespace hydrolib::Logger
                 }
             }
 
-            void Push(const uint8_t *source, std::size_t length) const
+            hydrolib_ReturnCode Push(const uint8_t *source, std::size_t length) const
             {
+                hydrolib_ReturnCode self_res = HYDROLIB_RETURN_OK;
+                hydrolib_ReturnCode other_res = next_node_->Push(source, length);
                 if (stream_enable_)
                 {
-                    output_stream_.Push(source, length);
+                    self_res = output_stream_.Push(source, length);
                 }
-                next_node_->Push(source, length);
-            }
-
-            hydrolib_ReturnCode SetFormatString(unsigned depth, char *format_string)
-            {
-                if (depth == 0)
+                if (self_res != HYDROLIB_RETURN_OK)
                 {
-                    format_string_ = format_string;
-                    return HYDROLIB_RETURN_OK;
+                    return self_res;
                 }
-                if (next_node_)
+                if (other_res != HYDROLIB_RETURN_OK)
                 {
-                    return next_node_->SetFormatString(depth - 1, format_string);
+                    return other_res;
                 }
-                else
-                {
-                    return HYDROLIB_RETURN_FAIL;
-                }
+                return HYDROLIB_RETURN_OK;
             }
 
             hydrolib_ReturnCode SetLogFiltration(unsigned depth, unsigned logger_id, LogLevel level)
@@ -143,7 +137,6 @@ namespace hydrolib::Logger
         private:
             LogLevel level_filter_[MAX_LOGGERS_COUNT];
             Stream &output_stream_;
-            char *format_string_; // TODO Make CString (and add coping)
 
             bool stream_enable_;
 
@@ -154,23 +147,23 @@ namespace hydrolib::Logger
         class LogDistributingNode_<NextNode>
         {
         public:
-            consteval LogDistributingNode_(
-                [[maybe_unused]] char *format_string,
-                NextNode *next_node)
+            consteval LogDistributingNode_(NextNode *next_node)
                 : head_node(next_node)
             {
             }
 
         public:
             template <typename... Ts>
-            void Notify([[maybe_unused]] unsigned source_id,
+            bool Notify([[maybe_unused]] unsigned source_id,
                         [[maybe_unused]] LogLevel level) const // TODO workaround, need fix
             {
+                return true;
             }
 
-            void Push([[maybe_unused]] const uint8_t *source,
-                      [[maybe_unused]] std::size_t length) const
+            hydrolib_ReturnCode Push([[maybe_unused]] const uint8_t *source,
+                                     [[maybe_unused]] std::size_t length) const
             {
+                return HYDROLIB_RETURN_OK;
             }
 
             hydrolib_ReturnCode SetLogFiltration(
@@ -190,32 +183,16 @@ namespace hydrolib::Logger
             NextNode *head_node;
         };
 
-    public:
-        consteval LogDistributor(char *format_string, Streams &...streams);
-
-    public:
-        template <typename... Ts>
-        void Notify(unsigned source_id, Log &log, Ts... params) const
-        {
-            if (distributing_list_.head_node->Notify(source_id, log.level))
-            {
-                output_stream_.Open();
-                log.ToBytes(format_string_, *distributing_list_.head_node, params...);
-                output_stream_.Close();
-            }
-        }
-
-        hydrolib_ReturnCode SetFilter(unsigned stream_number, unsigned logger_id,
-                                      LogLevel level);
-        hydrolib_ReturnCode SetAllFilters(unsigned logger_id, LogLevel level);
-
     private:
         LogDistributingNode_<LogDistributingNode_<void>, Streams...> distributing_list_;
+
+        char *format_string_; // TODO Make CString (and add coping)
     };
 
     template <SynchronousByteStreamConcept... Streams>
     consteval LogDistributor<Streams...>::LogDistributor(char *format_string, Streams &...streams)
-        : distributing_list_(format_string, nullptr, streams...)
+        : distributing_list_(nullptr, streams...),
+          format_string_(format_string)
     {
     }
 
