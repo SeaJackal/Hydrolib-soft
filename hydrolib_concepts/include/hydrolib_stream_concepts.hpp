c3164e5230b4d889818ad23a08d31ee2d29dae14
Vovdenko Dmitriy <vov-dm-an@yandex-team.ru>
2025-06-07 01:57:57 +0300

    Stream fixes

--- hydrolib_concepts/include/hydrolib_stream_concepts.hpp
+++ hydrolib_concepts/include/hydrolib_stream_concepts.hpp
@@ -12,7 +12,7 @@ namespace hydrolib::concepts::stream
 template <typename T>
 concept ByteWritableStreamConceptPOSIX =
     requires(T stream, const void *source, unsigned length) {
-        { write(stream, source, length) } -> std::same_as<int>;
+        { write(stream, source, length) } -> std::convertible_to<int>;
     };
 
 template <typename T>
@@ -28,7 +28,7 @@ concept ByteWritableStreamConcept =
 template <typename T>
 concept ByteReadableStreamConcept =
     requires(T stream, void *dest, unsigned length) {
-        { read(stream, dest, length) } -> std::same_as<int>;
+        { read(stream, dest, length) } -> std::convertible_to<int>;
     };
 } // namespace hydrolib::concepts::stream
 
