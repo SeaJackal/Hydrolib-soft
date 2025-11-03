dcf17d49ad27edcc8573bd52270abe073d4d4cc0
SeaJackal <vov-dm-an@gmail.com>
2025-11-03 19:16:18 +0000

    Prompt added

--- hydrolib_shell_interpreter/include/hydrolib_shell.hpp
+++ hydrolib_shell_interpreter/include/hydrolib_shell.hpp
@@ -12,12 +12,17 @@ template <concepts::stream::ByteFullStreamConcept Stream, typename Func,
 class Shell
 {
 public:
+    static constexpr std::string_view kPrompt = "\r\nhydrosh > ";
+
+public:
     constexpr Shell(Stream &stream, Map &handlers);
 
 public:
+    void Init();
     hydrolib::ReturnCode Process();
 
 private:
+    Stream &stream_;
     Terminal<Stream> terminal_;
     Interpreter<Func, Map> interpreter_;
 
@@ -27,8 +32,18 @@ private:
 template <concepts::stream::ByteFullStreamConcept Stream, typename Func,
           CommandMapConcept<Func> Map>
 constexpr Shell<Stream, Func, Map>::Shell(Stream &stream, Map &handlers)
-    : terminal_(stream), interpreter_(handlers), last_error_code_(0)
+    : stream_(stream),
+      terminal_(stream),
+      interpreter_(handlers),
+      last_error_code_(0)
 {
+}
+
+template <concepts::stream::ByteFullStreamConcept Stream, typename Func,
+          CommandMapConcept<Func> Map>
+void Shell<Stream, Func, Map>::Init()
+{
+    write(stream_, kPrompt.data(), kPrompt.length());
 }
 
 template <concepts::stream::ByteFullStreamConcept Stream, typename Func,
@@ -42,6 +57,7 @@ hydrolib::ReturnCode Shell<Stream, Func, Map>::Process()
     }
     auto command = terminal_.GetCommand();
     last_error_code_ = interpreter_.Process(command);
+    write(stream_, kPrompt.data(), kPrompt.length());
     if (last_error_code_ != 0)
     {
         return hydrolib::ReturnCode::ERROR;
