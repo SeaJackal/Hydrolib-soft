de0841a6e89f505328bcfb4a63c8a8f187bddc68
SeaJackal <vov-dm-an@gmail.com>
2025-12-06 11:19:51 +0000

    Added command map

---
+++ hydrolib_shell_commands/include/hydrolib_command_map.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <optional>
+#include <string_view>
+
+#include "hydrolib_echo.hpp"
+#include "hydrolib_cat.hpp"
+
+namespace hydrolib::shell
+{
+class CommandMap
+{
+public:
+    using CommandType = decltype(&hydrolib::shell::Echo);
+
+public:
+    std::optional<CommandType> operator[](std::string_view command)
+    {
+        if (command == "echo")
+        {
+            return hydrolib::shell::Echo;
+        }
+        else if (command == "cat")
+        {
+            return hydrolib::shell::Cat;
+        }
+        return std::nullopt;
+    }
+};
+
+inline CommandMap command_map;
+} // namespace hydrolib::shell
