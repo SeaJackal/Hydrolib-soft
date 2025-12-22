2d2e0fb64068c7cb1e1beee94be93afeba9da25e
klegot <redwolf.2007@mail.ru>
2025-12-22 22:08:14 +0000

    fixed ThrusterDevice again

--- hydrolib_device/include/hydrolib_thruster_device.hpp
+++ hydrolib_device/include/hydrolib_thruster_device.hpp
@@ -18,4 +18,25 @@ public:
     virtual hydrolib::ReturnCode SetSpeed(int speed) = 0;
     virtual int GetSpeed() = 0;
 };
+
+template <typename Thruster>
+class ThrusterDevice : public IThruster
+{
+public:
+    ThrusterDevice(std::string_view name, Thruster &thruster);
+
+    hydrolib::ReturnCode SetSpeed(int speed) override;
+    int GetSpeed() override;
+
+private:
+    Thruster &thruster_;
+    int current_speed_;
+};
+
+template <typename Thruster>
+ThrusterDevice<Thruster>::ThrusterDevice(std::string_view name,
+                                         Thruster &thruster)
+    : IThruster(name), thruster_(thruster)
+{
+}
 } // namespace hydrolib::device
