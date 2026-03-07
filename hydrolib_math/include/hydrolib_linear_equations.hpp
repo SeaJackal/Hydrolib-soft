bc6b9aa5312b360d319369e69d7a4ce1466ad0a9
SeaJackal <vov-dm-an@gmail.com>
2026-03-07 14:40:14 +0000

    Fixed unsolvable case

--- hydrolib_math/include/hydrolib_linear_equations.hpp
+++ hydrolib_math/include/hydrolib_linear_equations.hpp
@@ -13,28 +13,28 @@ template <ArithmeticConcept Number, int N>
 constexpr void SolveLinear(std::array<std::array<Number, N>, N> coefficients,
                            std::array<Number, N> free_terms,
                            std::array<Number, N>& results) {
-  if (coefficients[0][0] == 0) [[unlikely]] {
-    for (int i = 1; i < N; i++) {
-      if (coefficients[i][0] != 0) {
-        for (int j = 0; j < N; j++) {
-          coefficients[0][j] += coefficients[i][j];
-        }
-        break;
-      }
-    }
-  }
   for (int i = 0; i < N - 1; i++) {
     if (coefficients[i][i] == 0) [[unlikely]] {
+      bool found = false;
       for (int j = i + 1; j < N; j++) {
-        coefficients[i][j] += coefficients[i - 1][j];
+        if (coefficients[i][i] != 0) {
+          for (int k = i; k < N; k++) {
+            coefficients[k][j] += coefficients[k][i] / coefficients[i][i];
+          }
+          free_terms[i] += free_terms[j];
+          found = true;
+          break;
+        }
       }
-      free_terms[i] += free_terms[i - 1];
-    } else {
-      for (int j = i + 1; j < N; j++) {
-        coefficients[i][j] /= coefficients[i][i];
+      if (!found) {
+        return;
       }
-      free_terms[i] /= coefficients[i][i];
     }
+    for (int j = i + 1; j < N; j++) {
+      coefficients[i][j] /= coefficients[i][i];
+    }
+    free_terms[i] /= coefficients[i][i];
+
     for (int k = i + 1; k < N; k++) {
       for (int j = i + 1; j < N; j++) {
         coefficients[k][j] -= coefficients[k][i] * coefficients[i][j];
