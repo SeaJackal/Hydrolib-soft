86b9c6da20466e9780d38721a3f6579c8dbb194d
SeaJackal <vov-dm-an@gmail.com>
2026-03-25 13:55:43 +0000

    Reworked PID

--- hydrolib_pid/tests/test_hydrolib_pid.cpp
+++ hydrolib_pid/tests/test_hydrolib_pid.cpp
@@ -4,67 +4,98 @@
 #include <iostream>
 #include <numbers>
 
+#include "hydrolib_fixed_point.hpp"
 #include "hydrolib_pid.hpp"
 
 using namespace hydrolib;
 
 TEST(TestPID, HarmonicTest) {
-  constexpr unsigned freq_hz = 100;
-  constexpr unsigned test_time_s = 5;
+  constexpr unsigned kFreqHz = 128;
+  constexpr unsigned kTestTimeS = 5;
 
-  constexpr unsigned control_amplitude = 1000;
-  constexpr unsigned control_freq_hz = 1;
+  constexpr unsigned kControlAmplitude = 10;
+  constexpr unsigned kControlFreqHz = 1;
 
-  constexpr unsigned p = 1;
-  constexpr unsigned i = 100;
-  constexpr unsigned divide_shift = 12;
+  constexpr int kPCoef = 1;
+  constexpr int kICoef = 100;
+  constexpr int kDCoef = 10;
 
-  constexpr double freq_rad = control_freq_hz * 2 * std::numbers::pi;
+  constexpr double kControlFreqRad = kControlFreqHz * 2 * std::numbers::pi;
 
-  controlling::PID<freq_hz> pid;
-  pid.SetP(p << divide_shift);
-  pid.SetI(i << divide_shift);
-  pid.SetDivideShift(divide_shift);
+  constexpr double kPeriod = 1.0 / kFreqHz;
+  constexpr double kDifferentialPeriod = kPeriod * 4;
 
-  for (unsigned t = 0; t < freq_hz * test_time_s; t++) {
-    double sin_value = std::sin(static_cast<double>(t) * freq_rad / freq_hz);
-    double cos_value = std::cos(static_cast<double>(t) * freq_rad / freq_hz);
-    double control = control_amplitude * sin_value;
-    double target_output = p * control_amplitude * sin_value -
-                           i * control_amplitude * cos_value / freq_rad +
-                           i * control_amplitude / freq_rad;
-    int real_output = pid.Process(control);
+  controlling::PID<kFreqHz, math::FixedPointBase> pid;
+  pid.SetP(kPCoef);
+  pid.SetI(kICoef);
+  pid.SetD(kDCoef);
 
-    // std::cout << target_output << " : " << real_output << "\n";
+  for (unsigned time = 0; time < kFreqHz * kTestTimeS; time++) {
+    double sin_value =
+        std::sin(static_cast<double>(time) * kControlFreqRad / kFreqHz);
+    double cos_value =
+        std::cos(static_cast<double>(time) * kControlFreqRad / kFreqHz);
+    double control = kControlAmplitude * sin_value;
+    constexpr double kCosAmplitude =
+        kControlAmplitude *
+        (-kICoef / kControlFreqRad +
+         kControlFreqRad *
+             (kDCoef - kICoef * kDifferentialPeriod * kDifferentialPeriod)) /
+        (kDifferentialPeriod * kDifferentialPeriod * kControlFreqRad *
+             kControlFreqRad +
+         1);
+    constexpr double kSinAmplitude =
+        kControlAmplitude *
+        (kPCoef + (kDCoef + kPCoef * kDifferentialPeriod) *
+                      kDifferentialPeriod * kControlFreqRad * kControlFreqRad) /
+        (kDifferentialPeriod * kDifferentialPeriod * kControlFreqRad *
+             kControlFreqRad +
+         1);
+    double target_output =
+        (kSinAmplitude * sin_value + kCosAmplitude * cos_value) +
+        (kControlAmplitude * kICoef / kControlFreqRad);
+    auto real_output = pid.Process(math::FixedPointBase(
+        static_cast<int>(control * (1 << math::FixedPointBase::kFractionBits)),
+        (1 << math::FixedPointBase::kFractionBits)));
 
-    EXPECT_LT(std::abs(target_output - real_output), control_amplitude * 0.05);
+    // std::cout << target_output << " : " << static_cast<double>(real_output)
+    //           << "\n";
+
+    if (time >= kFreqHz) {
+      EXPECT_LT(std::abs(target_output - static_cast<double>(real_output)),
+                kControlAmplitude * 0.06);
+    }
   }
 }
 
 TEST(TestPID, ControlTest) {
-  constexpr unsigned freq_hz = 100;
-  constexpr unsigned test_time_s = 5;
+  constexpr int kFreqHz = 128;
+  constexpr unsigned kTestTimeS = 5;
 
-  constexpr unsigned control = 1000;
+  constexpr int control = 10;
   constexpr double tau = 1;
 
-  constexpr unsigned p = 100;
-  constexpr unsigned i = 200;
-  constexpr unsigned divide_shift = 10;
+  constexpr math::FixedPointBase kPCoef = 1;
+  constexpr math::FixedPointBase kICoef = 10;
+  constexpr math::FixedPointBase kDCoef = 0.1;
 
-  controlling::PID<freq_hz> pid;
-  pid.SetP(p << divide_shift);
-  pid.SetI(i << divide_shift);
-  pid.SetDivideShift(divide_shift);
+  controlling::PID<kFreqHz, math::FixedPointBase> pid;
+  pid.SetP(kPCoef);
+  pid.SetI(kICoef);
+  pid.SetD(kDCoef);
 
   double controlled_value = 0;
   double last_control = 0;
 
-  for (unsigned t = 0; t < freq_hz * test_time_s; t++) {
-    double current_control = pid.Process(control - controlled_value);
-    controlled_value = ((last_control + current_control) / freq_hz -
-                        (1 - 2 * tau) * controlled_value) /
-                       (2 * tau);
+  for (unsigned t = 0; t < kFreqHz * kTestTimeS; t++) {
+    double current_control =
+        static_cast<double>(pid.Process(math::FixedPointBase(
+            static_cast<int>((control - controlled_value) *
+                             (1 << math::FixedPointBase::kFractionBits)),
+            (1 << math::FixedPointBase::kFractionBits))));
+    controlled_value = ((last_control + current_control) / kFreqHz -
+                        (1.0 / kFreqHz - 2 * tau) * controlled_value) /
+                       (2 * tau + 1.0 / kFreqHz);
 
     last_control = current_control;
 
