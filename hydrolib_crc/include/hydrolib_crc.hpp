7de4a41d8ff467080fc68d1bf326859be30da5a7
SeaJackal <vov-dm-an@gmail.com>
2026-04-11 13:39:51 +0000

    Fixed crc

--- hydrolib_crc/include/hydrolib_crc.hpp
+++ hydrolib_crc/include/hydrolib_crc.hpp
@@ -1,21 +1,44 @@
 #pragma once
 
+#include <climits>
+#include <cstddef>
 #include <cstdint>
+#include <span>
 
 namespace hydrolib::crc {
-inline uint8_t CountCRC8(const uint8_t *buffer, unsigned length) {
-  uint16_t pol = 0x0700;
-  uint16_t crc = buffer[0] << 8;
-  for (uint8_t i = 1; i < length; i++) {
-    crc |= buffer[i];
-    for (uint8_t j = 0; j < 8; j++) {
-      if (crc & 0x8000) {
-        crc = (crc << 1 ^ pol);
-      } else {
-        crc = crc << 1;
-      }
+class CRC8 {
+ public:
+  static constexpr uint16_t kPolynomial = 0x0700;
+
+  void Next(std::byte byte);
+  void Next(std::span<const std::byte> data);
+  [[nodiscard]] std::byte Get();
+
+ private:
+  uint16_t crc_ = 0;
+};
+
+inline void CRC8::Next(std::byte byte) {
+  crc_ |= static_cast<uint8_t>(byte);
+  for (int i = 0; i < CHAR_BIT; i++) {
+    constexpr int kMaskForMostSignificantBit =
+        1 << ((CHAR_BIT * sizeof(uint16_t)) - 1);
+    if ((crc_ & kMaskForMostSignificantBit) != 0) {
+      crc_ = (crc_ << 1) ^ kPolynomial;
+    } else {
+      crc_ = crc_ << 1;
     }
   }
-  return crc >> 8;
+}
+
+inline void CRC8::Next(std::span<const std::byte> data) {
+  for (auto byte : data) {
+    Next(byte);
+  }
+}
+
+inline std::byte CRC8::Get() {
+  Next(std::byte(0));
+  return static_cast<std::byte>(crc_ >> CHAR_BIT);
 }
 }  // namespace hydrolib::crc
